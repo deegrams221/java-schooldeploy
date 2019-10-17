acd2393a598b09562bd689d37bfd5441a4dff351
Diana Grams <deegrams221@gmail.com>
2019-10-17 16:37:27 -0600

    fixed pom file

--- school/src/main/java/com/lambdaschool/school/handlers/RestExceptionHandler.java
+++ school/src/main/java/com/lambdaschool/school/handlers/RestExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
